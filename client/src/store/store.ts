171c7ad70eff1e6177fabfb0fbd2e76b091444d5
Makdoom Shaikh <makshaikh99@gmail.com>
2024-01-01 19:02:02 +0530

    feat: Configure redux-toolkit

---
+++ client/src/store/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// Create store using configurestore
+export const store = configureStore({
+  reducer: {
+    // counter: counterReducer,
+  },
+});
+
+// Rootstate return type while using with useSelector hook
+export type RootState = ReturnType<typeof store.getState>;
+
+// To dispatch
+export type AppDispatch = typeof store.dispatch;
