de7de7eeac52b4e6eb006554abe614971fb6f438
Makdoom Shaikh <makshaikh99@gmail.com>
2024-01-01 11:59:45 +0530

    feat: Configure express app with middlewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,21 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { PAYLOAD_LIMIT } from "./constant";
 
 // Initialize express app
 const app = express();
+
+// Middlewares
+app.use(express.json({ limit: PAYLOAD_LIMIT }));
+app.use(express.urlencoded({ extended: true, limit: PAYLOAD_LIMIT }));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
 export default app;
--- server/src/constant/index.ts
+++ server/src/constant/index.ts
@@ -1,1 +1,2 @@
 export const DB_NAME = "devnode";
+export const PAYLOAD_LIMIT = "16kb";
