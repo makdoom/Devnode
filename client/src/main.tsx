15ece26f84f0ac66b3256f9ffc99b0e7d1f0bde4
Makdoom Shaikh <makshaikh99@gmail.com>
2024-01-04 21:01:58 +0530

    feat: Add react-query and register user API

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,9 +5,14 @@ import "./index.css";
 import { store } from "./store/store.ts";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/index.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const clientQuery = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={clientQuery}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </Provider>
 );
