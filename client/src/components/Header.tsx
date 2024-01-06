85f1d1350b5bf0e7c8bc98e7f8a78644694ff323
Makdoom Shaikh <makshaikh99@gmail.com>
2024-01-06 20:08:20 +0530

    feat: Add frosted effect on header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
   }, [data]);
 
   return (
-    <div className="w-full py-2 p-4 sm:py-4 flex items-center justify-between border bg-white border-secondary sticky top-0 z-10 shadow-sm">
+    <div className="w-full py-2 p-4 sm:py-4 flex items-center justify-between border-b bg-[rgba(255,255,255,0.6)] backdrop-blur-[8px] border-secondary sticky top-0 z-10 shadow-sm">
       <div className="w-full">
         <h1
           className="font-semibold text-xl md:text-2xl cursor-pointer contents"
