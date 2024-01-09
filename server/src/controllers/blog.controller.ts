8fe82523e2a461163d81fb022dbaa06079030a18
Makdoom Shaikh <makshaikh99@gmail.com>
2024-01-09 22:09:44 +0530

    feat: Add get all blogs route

--- server/src/controllers/blog.controller.ts
+++ server/src/controllers/blog.controller.ts
@@ -3,6 +3,7 @@ import { asyncHandler } from "../utils/asynHandler";
 import ApiResponse from "../utils/ApiResponse";
 import { Blog } from "../models/blog.model";
 import { CustomRequest } from "../types/custom.types";
+import mongoose from "mongoose";
 
 const createBlog = asyncHandler(async (req: CustomRequest, res: Response) => {
   const { title, contents } = req.body;
@@ -18,4 +19,39 @@ const createBlog = asyncHandler(async (req: CustomRequest, res: Response) => {
     .json(new ApiResponse(200, "Blog created successfully", createdBlogPost));
 });
 
-export { createBlog };
+const getBlogs = asyncHandler(async (req: CustomRequest, res: Response) => {
+  const blogList = await Blog.aggregate([
+    {
+      $match: {
+        authorId: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "users", // the name of the users collection
+        localField: "authorId",
+        foreignField: "_id",
+        as: "author",
+      },
+    },
+    {
+      $unwind: "$author",
+    },
+    {
+      $project: {
+        title: 1,
+        createdAt: 1,
+        contents: 1,
+        "author.fullName": 1,
+        "author.email": 1,
+      },
+    },
+  ]);
+
+  console.log(blogList);
+  res
+    .status(201)
+    .json(new ApiResponse(200, "Blog created successfully", blogList));
+});
+
+export { createBlog, getBlogs };
