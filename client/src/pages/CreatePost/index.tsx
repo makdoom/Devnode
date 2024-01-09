515e13f120667f387c9765c52556f03b0bb9fb7d
Makdoom Shaikh <makshaikh99@gmail.com>
2024-01-09 22:25:48 +0530

    fix: component flickering when unauthorized user trying to access authorized pages

--- client/src/pages/CreatePost/index.tsx
+++ client/src/pages/CreatePost/index.tsx
@@ -1,11 +1,25 @@
 import Sidebar from "@/components/Sidebar";
 import WritingArea from "@/components/WritingArea";
-import { useState } from "react";
+import { useAppSelector } from "@/hooks/storeHook";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 const CreatePost = () => {
+  const { isAuthenticated } = useAppSelector((state) => state.authUser);
+
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
+  const navigate = useNavigate();
+
   const handleToggleSidebar = () => setIsSidebarOpen((prev) => !prev);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/");
+    }
+  }, []);
+
+  if (!isAuthenticated) return <div></div>;
 
   return (
     <div className="w-screen h-screen flex">
